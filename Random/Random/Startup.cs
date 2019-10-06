22222f5de1c6e9ae6bbdf6d663cd2f2e0d9a2f6e
Kaybee <u18066268@tuks.co.za>
2019-10-06 13:35:49 +0200

    Empty Random Project

---
+++ Random/Random/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Random.Startup))]
+namespace Random
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
